eead7a04dc4e6ce3f27ce43d080797170fe187a2
Victor Lopez (UST <U34006@ust-global.com>
2018-02-13 16:51:29 -0600

    Enhances made to the file.

--- app/navzilla/index.js
+++ app/navzilla/index.js
@@ -1,21 +1,14 @@
 'use strict';
 
 var express = require('express');
-// var app     = express();
-
-// var start = function () {
-//     app.listen(3000, () => console.info("Listening on port 3000!"));
-// }
-
+var server  = express();
 
 // app.get('/index', (req, res) => res.send("Hello World"));
 
-// module.exports = start;
-
 module.exports = function() {
-  let server = express(), start;
+  // let server = express(), start;
 
-  start = function() {
+  var start = function() {
         server.listen(3000, function () {
             console.log('Express server listening on port 3000');
         });
